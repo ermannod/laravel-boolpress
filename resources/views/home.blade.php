1463946fb63387f0d132fb0dd76243a11b8b6e1e
ermannod <erma-d@hotmail.com>
2020-02-18 19:14:25 +0100

    Boolpress

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.public')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-6 mx-auto">
+        <h1>Public post list</h1>
+      </div>
+    </div>
+
+  </div>
+
+@endsection
